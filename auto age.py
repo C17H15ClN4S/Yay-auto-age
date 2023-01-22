27ccff8b441d9ae1757d722f4479cc8cc7fd2c11
40054-69-1 <122404299+C17H15ClN4S@users.noreply.github.com>
2023-01-23 01:29:54 +0900

    Update auto age.py

--- auto age.py
+++ auto age.py
@@ -5,7 +5,7 @@ text = "text"
 post_id = "post_id"
 
 headers = {
-    'authorization': "Bearer " + TOKEN,
+    'authorization': TOKEN,
     'x-device-info': 'Yay 3.2.0 Web (Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36)',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
 }
